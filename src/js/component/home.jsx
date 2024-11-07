545f6a18196299b4c6aa9351ab467cbecb262434
Ivan Perez <pumi0751@gmail.com>
2024-11-07 11:07:11 +0000

    feat: finish the task

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,21 +1,15 @@
 import React, {useState} from "react";
 
 import {Light} from "./light";
-import {ButtonPurple} from "./button"
 
 //create your first component
 const Home = () => {
-	const [color, setColor] = useState('');
-	
 	return (
 		<>
 			<div className="traffic">
 				<Light />
 			</div>
 
-			<div className="buttonPurple" >
-               <ButtonPurple onClick={toggleColor}  />
-            </div>
 		</>
 	);
 };
